80b544fedf43a18a5644883761a3c6cc8eb7bda6
Angel <angelr4a1@gmail.com>
2022-09-02 01:37:01 -0500

    Write default header template

---
+++ gui/windows/main_window/ui_main_window.py
@@ -0,0 +1,15 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# AUTHOR: JOSE ANGEL RESENDIZ AVILES
+#
+# PROJECT CREATED WITH:  PySide6, astropy, scipy, matplotlib, numpy
+#
+# This project has MIT license and is intented to provide a Graphical
+# use interface to have insigts from fit images relatives to Coronal 
+# Mass Ejections (CME)
+#
+# Github profile: @Angelpacman
+# 
+# v0.0.1
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
