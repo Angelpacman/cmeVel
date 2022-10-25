4c73f6b5c1c6ec2d3e1191c77af5ae51fb9b4eaf
Angel <angelr4a1@gmail.com>
2022-10-25 12:31:24 -0500

    Add Butons to left menu

--- gui/widgets/py_push_button.py
+++ gui/widgets/py_push_button.py
@@ -4,8 +4,8 @@
 #
 # PROJECT CREATED WITH:  PySide6, astropy, scipy, matplotlib, numpy
 #
-# This project has MIT license and is intented to provide a Graphical
-# use interface to have insigts from fit images relatives to Coronal
+# This project has MIT license and is intended to provide a Graphical
+# use interface to have insights from fit images relatives to Coronal
 # Mass Ejections (CME)
 #
 # Github profile: @Angelpacman
@@ -20,20 +20,21 @@ import os
 # IMPORT QT CORE
 from qt_core import *
 
+
 class PyPushButton(QPushButton):
     def __init__(
-        self,
-        text = "",
-        height = 40,
-        minimum_widht = 50,
-        text_padding = 55,
-        text_color = "#c3ccdf",
-        icon_path = "",
-        icon_color = "#c3ccdf",
-        btn_color = "#44475a",
-        btn_hover = "#4f5368",
-        btn_pressed = "#282a36",
-        is_active = False
+            self,
+            text="",
+            height=40,
+            minimum_widht=50,
+            text_padding=55,
+            text_color="#c3ccdf",
+            icon_path="",
+            icon_color="#c3ccdf",
+            btn_color="#44475a",
+            btn_hover="#4f5368",
+            btn_pressed="#282a36",
+            is_active=False
     ):
 
         super().__init__()
@@ -57,22 +58,22 @@ class PyPushButton(QPushButton):
 
         # Set style
         self.set_style(
-            text_padding = self.text_padding,
-            text_color = self.text_color,
-            btn_color = self.btn_color,
-            btn_hover = self.btn_hover,
-            btn_pressed = self.btn_hover,
-            is_active = self.is_active
+            text_padding=self.text_padding,
+            text_color=self.text_color,
+            btn_color=self.btn_color,
+            btn_hover=self.btn_hover,
+            btn_pressed=self.btn_hover,
+            is_active=self.is_active
         )
 
     def set_style(
-        self,
-        text_padding = 55,
-        text_color = "#c3ccdf",
-        btn_color = "#44475a",
-        btn_hover = "#4f5368",
-        btn_pressed = "#282a36",
-        is_active = False
+            self,
+            text_padding=55,
+            text_color="#c3ccdf",
+            btn_color="#44475a",
+            btn_hover="#4f5368",
+            btn_pressed="#282a36",
+            is_active=False
     ):
 
         style = f"""
--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -24,6 +24,7 @@ from gui.pages.ui_pages import Ui_application_pages
 # IMPORT CUSTOM WIDGETS
 from gui.widgets.py_push_button import PyPushButton
 
+
 # MAIN WINDOW
 class UI_MainWindow(object):
     def setup_ui(self, parent):
@@ -43,7 +44,7 @@ class UI_MainWindow(object):
 
         # CREATE MAIN LAYOUT
         self.main_layout = QHBoxLayout(self.central_frame)
-        self.main_layout.setContentsMargins(0,0,0,0)
+        self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(0)
 
         # # # # # # # # # # # # # # # # 
@@ -56,41 +57,68 @@ class UI_MainWindow(object):
 
         # LEFT MENU LAYOUT
         self.left_menu_layout = QVBoxLayout(self.left_menu)
-        self.left_menu_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_layout.setContentsMargins(0, 0, 0, 0)
         self.left_menu_layout.setSpacing(0)
 
         # TOP FRAME MENU
         self.left_menu_top_frame = QFrame()
         self.left_menu_top_frame.setMinimumHeight(50)
         self.left_menu_top_frame.setObjectName("left_menu_top_frame")
-        self.left_menu_top_frame.setStyleSheet("#left_menu_top_frame {background-color: red;}")
+        # self.left_menu_top_frame.setStyleSheet("#left_menu_top_frame {background-color: red;}")
 
         # TOP FRAME LAYOUT
         self.left_menu_top_layout = QVBoxLayout(self.left_menu_top_frame)
-        self.left_menu_top_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_top_layout.setContentsMargins(0, 0, 0, 0)
         self.left_menu_top_layout.setSpacing(0)
 
         # TOP BUTTONS
         self.toggle_button = PyPushButton(
-            text = "Ocultar menu"
+            text="Ocultar menu",
+            icon_path="menu.svg"
         )
         self.btn_1 = PyPushButton(
-            text = "Inicio",
-            is_active = True
+            text="Inicio",
+            is_active=True,
+            icon_path="home.svg"
         )
         self.btn_2 = PyPushButton(
-            text = "Cargar datos"
+            text="Cargar datos",
+            icon_path="folder.svg"
+        )
+        self.btn_3_cone=PyPushButton(
+            text="Dibujar Cono",
+            icon_path="cono.svg"
+        )
+        self.btn_4_curves = PyPushButton(
+            text="Mejora de Curvas",
+            icon_path="curves.svg"
+        )
+        self.btn_5_gaussFit = PyPushButton(
+            text="Ajustar Gaussianas",
+            icon_path="gauss.svg"
+        )
+        self.btn_6_checkGraphs = PyPushButton(
+            text="CME Graph parameter",
+            icon_path="graph.svg"
+        )
+        self.btn_7_help = PyPushButton(
+            text="Ayuda (beta)",
+            icon_path="help.svg"
         )
 
         # ADD BUTTONS TO LAYOUT
         self.left_menu_top_layout.addWidget(self.toggle_button)
         self.left_menu_top_layout.addWidget(self.btn_1)
         self.left_menu_top_layout.addWidget(self.btn_2)
+        self.left_menu_top_layout.addWidget(self.btn_3_cone)
+        self.left_menu_top_layout.addWidget(self.btn_4_curves)
+        self.left_menu_top_layout.addWidget(self.btn_5_gaussFit)
+        self.left_menu_top_layout.addWidget(self.btn_6_checkGraphs)
 
         # # # # # # # # # # # # # # # # 
         # MENU SPACER
         # # # # # # # # # # # # # # # # 
-        self.left_menu_spacer = QSpacerItem(20,20, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        self.left_menu_spacer = QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         # # # # # # # # # # # # # # # # 
         # BOTTOM FRAME MENU
@@ -98,17 +126,17 @@ class UI_MainWindow(object):
         self.left_menu_bottom_frame = QFrame()
         self.left_menu_bottom_frame.setMinimumHeight(50)
         self.left_menu_bottom_frame.setObjectName("left_menu_bottom_frame")
-        self.left_menu_bottom_frame.setStyleSheet("#left_menu_bottom_frame {background-color: red;} ")
+        # self.left_menu_bottom_frame.setStyleSheet("#left_menu_bottom_frame {background-color: red;} ")
 
         # BOTTOM FRAME LAYOUT
         self.left_menu_bottom_layout = QVBoxLayout(self.left_menu_bottom_frame)
-        self.left_menu_bottom_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_bottom_layout.setContentsMargins(0, 0, 0, 0)
         self.left_menu_bottom_layout.setSpacing(0)
 
         # BOTTOM BUTTONS
         self.help_button = PyPushButton(
             text="Ayuda"
-            )
+        )
 
         # ADD BOTTOM BUTTONS TO LAYOUT
         self.left_menu_bottom_layout.addWidget(self.help_button)
@@ -132,66 +160,65 @@ class UI_MainWindow(object):
         self.content = QFrame()
         self.content.setStyleSheet("background-color: #282a36")
 
-            # Content Layout
+        # Content Layout
         self.content_layout = QVBoxLayout(self.content)
-        self.content_layout.setContentsMargins(0,0,0,0)
+        self.content_layout.setContentsMargins(0, 0, 0, 0)
         self.content_layout.setSpacing(0)
 
-            # Top bar
+        # Top bar
         self.top_bar = QFrame()
         self.top_bar.setMinimumHeight(25)
         self.top_bar.setMaximumHeight(25)
         self.top_bar.setStyleSheet("background-color: #21232d; color: #6272a4")
         self.top_bar_layout = QHBoxLayout(self.top_bar)
-        self.top_bar_layout.setContentsMargins(10,0,10,0)
+        self.top_bar_layout.setContentsMargins(10, 0, 10, 0)
 
-            # left label
+        # left label
         self.top_label_left = QLabel("cmeVel")
 
-            # top spacer
-        self.top_spacer = QSpacerItem(20,20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        # top spacer
+        self.top_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-            # right label
+        # right label
         self.top_label_right = QLabel("Pagina inicial")
         self.top_label_right.setStyleSheet("font: 700 9pt 'Segoe UI'")
 
-            # add to layout
+        # add to layout
         self.top_bar_layout.addWidget(self.top_label_left)
         self.top_bar_layout.addItem(self.top_spacer)
         self.top_bar_layout.addWidget(self.top_label_right)
 
-            # Application to pages
+        # Application to pages
         self.pages = QStackedWidget()
         self.pages.setStyleSheet("font-size: 12pt; color: #f8f8f2")
         self.ui_pages = Ui_application_pages()
         self.ui_pages.setupUi(self.pages)
         self.pages.setCurrentWidget(self.ui_pages.page_1)
 
-            # Bottom bar
+        # Bottom bar
         self.bottom_bar = QFrame()
         self.bottom_bar.setMinimumHeight(25)
         self.bottom_bar.setMaximumHeight(25)
         self.bottom_bar.setStyleSheet("background-color: #21232d; color: #6272a4")
 
         self.bottom_bar_layout = QHBoxLayout(self.bottom_bar)
-        self.bottom_bar_layout.setContentsMargins(10,0,10,0)
+        self.bottom_bar_layout.setContentsMargins(10, 0, 10, 0)
 
-            # left label
+        # left label
         self.bottom_label_left = QLabel("Desarrollado por: Jose Angel Resendiz Aviles")
 
-            # bottom spacer
-        self.bottom_spacer = QSpacerItem(20,20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+        # bottom spacer
+        self.bottom_spacer = QSpacerItem(20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
 
-            # right label
+        # right label
         self.bottom_label_right = QLabel("2022")
 
-            # add to layout
+        # add to layout
         self.bottom_bar_layout.addWidget(self.bottom_label_left)
         self.bottom_bar_layout.addItem(self.bottom_spacer)
         self.bottom_bar_layout.addWidget(self.bottom_label_right)
 
-
-            # Add to content layout
+        # Add to content layout
         self.content_layout.addWidget(self.top_bar)
         self.content_layout.addWidget(self.pages)
         self.content_layout.addWidget(self.bottom_bar)
